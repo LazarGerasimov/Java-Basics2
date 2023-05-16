d7cd5f0538fa033bb50455daece8f4f7a69c3ce0
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-16 13:31:58 +0000

    test changes

--- 01.JavaBasics/Demo1.java
+++ 01.JavaBasics/Demo1.java
@@ -2,5 +2,8 @@ class Demo1 {
   public static void main(String[] args) {
     System.out.println("Test123");
     System.out.println("Test123");
+    System.out.println("Test123");
+    System.out.println("Test123");
+    System.out.println("Test123");
   }
 }
