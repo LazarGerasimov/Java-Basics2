486b55adde021833675425bbaf2c25dd44f3aa36
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-16 18:35:58 +0000

    upd demo3: shorten syntax

--- 01.JavaBasics/Demo3.java
+++ 01.JavaBasics/Demo3.java
@@ -4,9 +4,14 @@ public class Demo3 {
   public static void main(String[] arg) {
 
     Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    int number = Integer.parseInt(input);
-    int result = number * 2;
+    
+    // String input = scanner.nextLine();
+    // int number = Integer.parseInt(input);
+
+    int inputNumber = Integer.parseInt(scanner.nextLine());
+    
+    int result = inputNumber * 2;
+    
     System.out.println(result);
     scanner.close();
   }
