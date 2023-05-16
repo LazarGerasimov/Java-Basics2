1d37a169dbdd3eb2036ce25e76771c94c21d2d28
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-16 15:06:32 +0000

    upd Demo2; add Scanner

--- 01.JavaBasics/Demo2.java
+++ 01.JavaBasics/Demo2.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Demo2 {
   public static void main(String[] arg) {
 
-  String random = "a";
-    System.out.println(random);}
+    // String randomStr = "a";
+    // double randomNum = 5;
+    // System.out.println(randomStr);
+    // System.out.println(randomNum);
+
+    Scanner scanner = new Scanner(System.in);
+    
+    try {
+      String name = scanner.nextLine();
+      System.out.println(name);
+    } finally {
+      in.close();
+    }
+  }
 }
