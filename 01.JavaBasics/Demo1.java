a100dd2a7b9faa08378c32cb3eb71c75d44de5f8
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-16 13:46:48 +0000

    remove excessive code

--- 01.JavaBasics/Demo1.java
+++ 01.JavaBasics/Demo1.java
@@ -1,9 +1,5 @@
 class Demo1 {
   public static void main(String[] args) {
     System.out.println("Test123");
-    System.out.println("Test123");
-    System.out.println("Test123");
-    System.out.println("Test123");
-    System.out.println("Test123");
   }
 }
