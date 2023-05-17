c680d7313238b9fed8d84c94a0ea5346b17ebb9f
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-17 12:44:45 +0000

    format parameters example - %

--- 01.JavaBasics/Demo4.java
+++ 01.JavaBasics/Demo4.java
@@ -5,13 +5,20 @@ public class Demo4 {
 
     Scanner scanner = new Scanner(System.in);
 
-    String name = scanner.nextLine();
+    // String name = scanner.nextLine();
 
     // System.out.print("Hello, ");
     // System.out.print(name);
     // System.out.println("!");
 
-    System.out.println("Hello " + name + "!");
+    // System.out.println("Hello " + name + "!");
+
+    String firstName = scanner.nextLine();
+    String lastName = scanner.nextLine();
+    int age = Integer.parseInt(scanner.nextLine());
+    String town = scanner.nextLine();
+
+    System.out.printf("%s %s, %d, %s", firstName, lastName, age, town);
 
     scanner.close();
   }
