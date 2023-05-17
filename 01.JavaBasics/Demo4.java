535172a0537877b2d88f519016251c4f136d0d81
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-17 12:09:18 +0000

    concatenation test

--- 01.JavaBasics/Demo4.java
+++ 01.JavaBasics/Demo4.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Demo4 {
   public static void main(String[] arg) {
 
-    Scanner scanner = new Scanner();
+    Scanner scanner = new Scanner(System.in);
+
+    String name = scanner.nextLine();
+
+    System.out.print("Hello, ");
+    System.out.print(name);
+    System.out.println("!");
 
     scanner.close();
   }
