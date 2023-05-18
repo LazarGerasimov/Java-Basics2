71ad29c22e7d6e6fdb81f9742ea55c93131cf54f
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-18 20:10:59 +0000

    comparing value vs reference

--- 01.JavaBasics/Demo5.java
+++ 01.JavaBasics/Demo5.java
@@ -4,9 +4,13 @@ public class Demo5 {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    
-    
+
+    String a = scanner.nextLine();
+    String b = scanner.nextLine();
+
+    System.out.println(a.equals(b)); // returns true; compares values
+    System.out.println(a == b); // return false; compares reference
 
     scanner.close();
-    }
+  }
 }
