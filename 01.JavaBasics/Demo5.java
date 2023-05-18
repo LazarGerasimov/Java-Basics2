5967e13458518dc9d59b46cbaf7f5b8206f5263d
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-18 20:26:25 +0000

    String Pool; JVM Heap Memory;

--- 01.JavaBasics/Demo5.java
+++ 01.JavaBasics/Demo5.java
@@ -8,8 +8,14 @@ public class Demo5 {
     String a = scanner.nextLine();
     String b = scanner.nextLine();
 
-    System.out.println(a.equals(b)); // returns true; compares values
-    System.out.println(a == b); // return false; compares reference
+    String c = "5";
+    String d = "5";
+
+    System.out.println(a.equals(b)); // returns true; compares values regardless of reference 
+    System.out.println(a == b); // return false (as data comes from input); compares reference
+    System.out.println(c == d); // returns true (as data comes from String Pool; compares reference);
+
+    // String Pool == Storage in JVM Heap Memory; 
 
     scanner.close();
   }
