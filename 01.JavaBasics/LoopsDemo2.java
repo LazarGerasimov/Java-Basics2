8a49053002cc7ad589c0a336ade8da9060124ca7
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-05-25 15:41:33 +0000

    add LoopsDemo2

---
+++ 01.JavaBasics/LoopsDemo2.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+public class LoopsDemo2 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    
+
+    scanner.close();
+  }
+}
